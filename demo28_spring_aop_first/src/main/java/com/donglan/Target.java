0b336f1f71e9598b4a3362472eae08a3ea2c8175
zhangyanyv <2753779669@qq.com>
2021-12-30 13:17:11 +0800

    springAOP入门

---
+++ demo28_spring_aop_first/src/main/java/com/donglan/Target.java
@@ -0,0 +1,7 @@
+package com.donglan;
+
+public class Target implements TargetInterface{
+    public void method() {
+        System.out.println("Target method running... ");
+    }
+}
---
+++ demo28_spring_aop_first/src/main/java/com/donglan/TargetInterface.java
@@ -0,0 +1,5 @@
+package com.donglan;
+
+public interface TargetInterface {
+    void method();
+}
